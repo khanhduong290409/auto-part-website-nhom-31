9fea1dc1feb80cdbc8c8c1c6cfc51522ab4fdc3c
khanhduong290409 <khanhduong290409@gmail.com>
2024-11-21 18:18:43 +0700

    hien thi hinh anh chi tiet san pham va sua code css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
+/**  Increment and Decrement input Number **/
+const plus = document.getElementById('plus')
+const minus = document.getElementById('minus')
+const totalItems = document.getElementById('totalItems')
+
+plus.addEventListener('click', () => {
+  totalItems.value++
+})
+
+minus.addEventListener('click', () => {
+  totalItems.value--
+  if (totalItems.value == 0 || totalItems.value < 0) {
+    totalItems.value = 1
+  }
+})
+
+totalItems.addEventListener('change', () => {
+  if (totalItems.value == 0 || totalItems.value < 0) {
+    totalItems.value = 1
+  }
+})
+
+/** change emphasis product Picture **/
+
+const smallpictures = document.querySelectorAll('.smallpictures')
+const emphasisPicture = document.getElementById('emphasisPicture')
+
+smallpictures.forEach(picture => {
+  picture.addEventListener('click', (e) => {
+    const src = picture.src.replace('-thumbnail', '')
+    const currentActivePicture = document.querySelector('.active')
+
+    currentActivePicture.classList.remove('active')
+    picture.classList.add('active')
+
+
+    emphasisPicture.src = src
+  })
+})
+
 $(document).ready(function () {
 	$(".carousel").carousel({
 		interval: 4000,
